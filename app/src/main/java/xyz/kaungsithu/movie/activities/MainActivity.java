9b480f3ca8772ac844f53cc515adce4fc7c9c249
Kaung Sithu <kaungsithu930@gmail.com>
2017-12-16 01:13:14 +0630

    blah

--- app/src/main/java/xyz/kaungsithu/movie/activities/MainActivity.java
+++ app/src/main/java/xyz/kaungsithu/movie/activities/MainActivity.java
@@ -1,28 +1,54 @@
-package xyz.kaungsithu.movie.activities;
+package xyz.zzp.ass_movie.activities.activities.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import xyz.kaungsithu.movie.*;
+import xyz.kaungsithu.*;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.rv_movie)
+    RecyclerView rvMovie;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        ButterKnife.bind(this,this);
         setSupportActionBar(toolbar);
+
+        MoviesAdapter moviesAdapter = new MoviesAdapter();
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext(),
+                LinearLayoutManager.VERTICAL,false);
+
+        rvMovie.setLayoutManager(linearLayoutManager);
+        rvMovie.setAdapter(moviesAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_SHORT)
                         .setAction("Action", null).show();
             }
         });
